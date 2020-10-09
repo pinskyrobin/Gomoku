51caa4b246d26d446147e8b023819404cc0161d5
x568059888 <xu568059888@163.com>
2020-10-09 15:23:08 +0800

    简化部分代码.

--- main.py
+++ main.py
@@ -4,13 +4,9 @@ from window import GomokuWindow
 import sys
 
 
-def main():
-    # g = Gomoku()
-    # g.play()
-    app = QApplication(sys.argv)
-    ex = GomokuWindow()
-    sys.exit(app.exec_())
+app = QApplication(sys.argv)
+ex = GomokuWindow()
+ex.show()
+sys.exit(app.exec_())
 
 
-if __name__ == '__main__':
-    main()
