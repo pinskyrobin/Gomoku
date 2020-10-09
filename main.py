0d260ea2b301f41386090899a155d86ae5b7d42f
x568059888 <xu568059888@163.com>
2020-10-09 10:55:20 +0800

    init.

---
+++ main.py
@@ -0,0 +1,16 @@
+from PyQt5.QtWidgets import QApplication
+from game import Gomoku
+from window import GomokuWindow
+import sys
+
+
+def main():
+    # g = Gomoku()
+    # g.play()
+    app = QApplication(sys.argv)
+    ex = GomokuWindow()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
