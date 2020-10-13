a45398f0dbe1a1d65793641f7b31eb94cd0f12a2
dinglinghui <dlinuigh@gmail.com>
2020-10-14 00:49:39 +0800

    finish the alpha-beta pure the search tree, this may work.

--- dinglinghui/GetThePoint.py
+++ dinglinghui/GetThePoint.py
@@ -1,4 +1,4 @@
-from .Macro import BOARD_BUILTIN
+from .Macro import BOARD_BUILTIN, PLAYER
 from .Node import Node
 
 
@@ -15,34 +15,64 @@ def search_line(board, row):
     return node_list
 
 
+def update_board(board, node_list):
+    for i in node_list:
+        board.chessboard[i.y][i.x] = i.kind
+    return board
+
+
+def restore_board(board, node_list):
+    for i in node_list:
+        board.chessboard[i.y][i.x] = BOARD_BUILTIN.BLANK
+
+
 def search_node(board, node_list):
-    node_list = []
-    board.chessboard[node.y][node.x] = BOARD_BUILTIN.BLACK
+    nodes = []
+    board = update_board(board, node_list)
     for i in range(BOARD_BUILTIN.HEIGHT):
-        node_list.extend(search_line(board, i))
+        nodes.extend(search_line(board, i))
+    restore_board(board, node_list)
+    return nodes
 
-    return node_list
+
+def evaluate_point(board):
+    return 1
 
 
-def create_nodes(board, node):
-    if node.depth <= BOARD_BUILTIN.DEPTH:
-        node.create_children(search_node(board, node))
+def create_nodes(board, node, node_list, alpha, beta, max_value, min_value, player):
+    # well, i can input chinese on my computer.
+    # player is refer to the one who is player, possibly is the one chess. not player is the opponent
+    if node.depth <= BOARD_BUILTIN.DEPTH & node.kind == player:
+        node.create_children(search_node(board, node_list))
         for i in node.children:
-            create_nodes(board, i)
+            node_list.append(node)
+            i.beta = evaluate_point(update_board(board, node_list))
+            restore_board(board, node_list)
+            beta += i.beta
+            if beta < min_value:
+                i.beta = beta
+                # sign to show this node is runnable
+                create_nodes(board, i, node_list, alpha, beta, max_value, beta, not player)
+            else:
+                node_list.pop()
+    elif node.depth <= BOARD_BUILTIN.DEPTH & node.kind != player:
+        node.create_children(search_node(board, node_list))
+        for i in node.children:
+            node_list.append(node)
+            i.alpha = evaluate_point(update_board(board, node_list))
+            restore_board(board, node_list)
+            alpha += i.value
+            if alpha > max_value:
+                create_nodes(board, i, node_list, alpha, beta, alpha, min_value, not player)
+            else:
+                node_list.pop()
+    else:
+        return node_list
 
 
-
-def create_tree(board):
+def create_and_pure_tree(board):
     node = Node(-1, -1)
-    create_nodes(board, node)
-    return node
+    nodelist = create_nodes(board, node, [], 0, 0, 0, 1000, 1)
+    return nodelist[0]
 
 
-def traverse_tree(node):
-    for i in node.children:
-        print("This is a node's x: " + i.x)
-        traverse_tree(i)
-
-
-def find_the_best(node):
-    return node
